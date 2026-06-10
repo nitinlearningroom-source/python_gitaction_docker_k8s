e1b8cbaf3570ec0ace56210ff7046c126bab37b1
Nitin Kulkarni <nitin.learning.room@gmail.com>
2026-06-10 20:15:32 +0530

    commiting file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello World from Nitin Kulkarni</h1>"
+    return "<h1>Hello World !! </h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
