b7842fe2acf519d81c388574bdc650784f89bf21
Nitin Kulkarni <nitin.learning.room@gmail.com>
2026-06-10 21:34:10 +0530

    commit python file

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return "<h1>Hello World !! </h1>"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
