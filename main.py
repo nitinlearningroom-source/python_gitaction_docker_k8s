94ef04fd2d6a400778b80b26f00f675c9c5dc3fa
Nitin Kulkarni <nitin.learning.room@gmail.com>
2026-06-13 18:59:54 +0530

    just

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1>First Docker and K8s application from GitHub Actions </h1>"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
