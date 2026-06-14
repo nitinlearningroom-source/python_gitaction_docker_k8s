03ed937f0685ae31bf22a236f9f9144aa635e5b1
Nitin Kulkarni <nitin.learning.room@gmail.com>
2026-06-14 15:50:30 +0530

    new changes

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+
 def home():
-    return "<h1>First Docker and K8s application from GitHub Actions </h1>"
+    return "<h1>First Docker and K8s application from GitHub Actions 06/14 </h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
